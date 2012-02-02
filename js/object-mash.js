c1d0f1cd85166c551efda0cf539206d055293da3
Duncan Cragg <netmash@cilux.org>
2012-02-02 20:13:52 +0000

    more excellent code

--- js/object-mash.js
+++ js/object-mash.js
@@ -22,6 +22,7 @@ function JSON2HTML() {
         getHTML: function(url,json){
             if(json.constructor!=Object) return 'Not an object!<br/>'+url+'<br/>'+json;
             if(this.isA('contact', json)) return this.getContactHTML(url,json);
+            if(this.isA('event',   json)) return this.getEventHTML(url,json);
             return this.getObjectHTML(url,json);
         },
         getAnyHTML: function(a){
@@ -42,43 +43,100 @@ function JSON2HTML() {
             var that = this;
             var rows = [];
             $.each(l, function(key,val){ rows.push(that.getAnyHTML(val)); });
-            if(rows.length >2) return '<div>'+rows.join(',</div>\n<div>')+'</div>\n';
+            if(rows.length >2) return '<p>'+rows.join(',</p>\n<p>')+'</p>\n';
             return rows.join(', ');
         },
         getStringHTML: function(s){
-           if(s.startsWith('http://')){
-               if(s.endsWith('json')) return '<a href="'+getBaseURL()+s.htmlEscape()+'"> &gt;&gt; </a>';
-               return '<a href="'+s.htmlEscape()+'"> &gt;&gt; </a>';
-           } else return s.htmlEscape();
+            if(s.startsWith('http://')){
+                if(s.endsWith('json')) return '<a href="'+getBaseURL()+s.htmlEscape()+'"> &gt;&gt; </a>';
+                return '<a href="'+s.htmlEscape()+'"> &gt;&gt; </a>';
+            } else return s.htmlEscape();
         },
         getContactHTML: function(url,json){
-           var rows=[];
-           rows.push('<div>Contact</div>');
-           rows.push('<a class="object" href="'+url+'">view source</a>\n');
-           rows.push('<div class="vcard">');
-           if(json.fullName !== undefined) rows.push('<h1 class="fn">'+json.fullName+'</h1>');
-           if(json.address  !== undefined) rows.push(this.getContactAddressHTML(json.address));
-           if(json.phone    !== undefined) rows.push('<p>Tel: <span class="tel">'+this.getListHTML(json.phone.work)+'</span></p>');
-           if(json.email    !== undefined) rows.push('<p>Email: <span class="email">'+json.email+'</span></p>');
-           if(json.webURL   !== undefined) rows.push('<p>Website: <a class="url" href="'+json.webURL+'">'+json.webURL+'</a></p>');
-           rows.push('</div>');
-           return rows.join('\n')+'\n';
+            var rows=[];
+            rows.push('<div>Contact</div>');
+            rows.push('<a class="object" href="'+url+'">view source</a>\n');
+            rows.push('<div class="vcard">');
+            if(json.fullName !== undefined) rows.push('<h2 class="fn">'+this.getAnyHTML(json.fullName)+'</h2>');
+            if(json.address  !== undefined) rows.push(this.getContactAddressHTML(json.address));
+            if(json.phone    !== undefined) rows.push(this.getContactPhoneHTML(json.phone));
+            if(json.email    !== undefined) rows.push('<p>Email: <span class="email">'+this.getAnyHTML(json.email)+'</span></p>');
+            if(json.webURL   !== undefined) rows.push('<p>Website: <a class="url" href="'+json.webURL+'">'+json.webURL+'</a></p>');
+            rows.push('</div>');
+            return rows.join('\n')+'\n';
         },
         getContactAddressHTML: function(address){
-           var rows=[];
-           rows.push('<div class="adr">');
-           if(address.street     !== undefined) rows.push('<p class="street-address">'+this.getListHTML(address.street)+'</p>');
-           if(address.locality   !== undefined) rows.push('<p class="locality">'+address.locality+'</p>');
-           if(address.region     !== undefined) rows.push('<p class="region">'+address.region+'</p>');
-           if(address.postalCode !== undefined) rows.push('<p class="postal-code">'+address.postalCode+'</p>');
-           if(address.country    !== undefined) rows.push('<p class="country-name">'+address.country+'</p>');
-           rows.push('</div>');
-           return rows.join('\n')+'\n';
+            var rows=[];
+            rows.push('<div class="adr">');
+            if(address.street     !== undefined) rows.push('<p class="street-address">'+this.getAnyHTML(address.street)+'</p>');
+            if(address.locality   !== undefined) rows.push('<p class="locality">'      +this.getAnyHTML(address.locality)+'</p>');
+            if(address.region     !== undefined) rows.push('<p class="region">'        +this.getAnyHTML(address.region)+'</p>');
+            if(address.postalCode !== undefined) rows.push('<p class="postal-code">'   +this.getAnyHTML(address.postalCode)+'</p>');
+            if(address.country    !== undefined) rows.push('<p class="country-name">'  +this.getAnyHTML(address.country)+'</p>');
+            rows.push('</div>');
+            return rows.join('\n')+'\n';
+        },
+        getContactPhoneHTML: function(phone){
+            var rows=[];
+            if(phone.constructor!==Object) rows.push('<p>Tel:    <span class="tel">'+this.getAnyHTML(phone)+'</span></p>');
+            else{
+            if(phone.mobile !== undefined) rows.push('<p>Mobile: <span class="tel">'+this.getAnyHTML(phone.mobile)+'</span></p>');
+            if(phone.home   !== undefined) rows.push('<p>Home:   <span class="tel">'+this.getAnyHTML(phone.home)+'</span></p>');
+            if(phone.work   !== undefined) rows.push('<p>Work:   <span class="tel">'+this.getAnyHTML(phone.work)+'</span></p>');
+            }
+            return rows.join('\n')+'\n';
+        },
+        getEventHTML: function(url,json){
+            var rows=[];
+            rows.push('<div>Event</div>');
+            rows.push('<a class="object" href="'+url+'">view source</a>\n');
+            rows.push('<div class="vevent">');
+            if(json.title    !== undefined) rows.push('<h2 class="summary">'+this.getAnyHTML(json.title)+'</h2>');
+            if(json.content  !== undefined) rows.push('<p class="description">'+this.getAnyHTML(json.content)+'</p>');
+            rows.push('<div>');
+            if(json.start    !== undefined) rows.push('<div class="dtstart" title="'+this.makeISODate(json.start)+'">'+this.makeNiceDate(json.start)+'</div>');
+            if(json.end      !== undefined) rows.push('<div class="dtend"   title="'+this.makeISODate(json.end  )+'">'+this.makeNiceDate(json.end  )+'</div>');
+            rows.push('</div>');
+            if(json.location !== undefined) rows.push(this.getEventLocationHTML(json.location));
+            if(json.attendees!== undefined) rows.push(this.getEventAttendeesHTML(json.attendees));
+            rows.push('</div>');
+            return rows.join('\n')+'\n';
+        },
+        getEventLocationHTML: function(url){
+            var rows=[];
+            rows.push('<h3>Location:</h3>');
+            rows.push('<a class="object" href="'+url+'">view source</a>\n');
+            rows.push('<div class="location vcard">');
+/*
+    <a class="opener fn" href="http://bletchleypark.org/address.html">Bletchley Park</a>
+    <div class="openable">
+      ..
+    </div>
+*/
+            rows.push('</div>');
+            return rows.join('\n')+'\n';
+        },
+        getEventAttendeesHTML: function(attendees){
+            var rows=[];
+            rows.push('<h3>Attendees:</h3>');
+            rows.push('<ul>');
+/*
+    <li class="attendee vcard fn"><a href="/staff/uid-55d3.html">Bob</a></li>
+    <li class="attendee vcard fn"><a href="uid-06-7d5d.html">Joe</a></li>
+*/
+            rows.push('</ul>');
+            return rows.join('\n')+'\n';
         },
         isA: function(type, json){
-           if(json.is===type) return true;
-           if(json.is !== undefined && json.is.constructor!==Array) return false;
-           return (json.is !== undefined && json.is.contains(type));
+            if(json.is===type) return true;
+            if(json.is !== undefined && json.is.constructor!==Array) return false;
+            return (json.is !== undefined && json.is.contains(type));
+        },
+        makeISODate: function(date){
+            return date;
+        },
+        makeNiceDate: function(date){
+            return date;
         }
     };
 };
