7b939e1309948c9093b4c700fef8044bee65a3c6
Duncan Cragg <object.network@cilux.org>
2012-02-03 15:33:37 +0000

    rewrote server to use streaming. thx to stackoverflow.

--- js/server.js
+++ js/server.js
@@ -1,48 +1,54 @@
 #!/usr/bin/node
 
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
 
-http.createServer(function (request, response) {
+var mimeTypes={
+    '.html': 'text/html',
+    '.jpeg': 'image/jpeg',
+    '.jpg':  'image/jpeg',
+    '.ico':  'image/x-icon',
+    '.png':  'image/png',
+    '.js':   'application/javascript',
+    '.json': 'application/json',
+    '.css':  'text/css'
+};
 
-    var filePath = '.' + request.url;
-    var q = filePath.indexOf('?');
-    if(q>=0) filePath = filePath.substring(0,q);
-    if(filePath.endethWith('/')) filePath += 'index.html';
+http.createServer(function(req, res) {
+    if(req.method !== 'GET'){
+        res.writeHead(400);
+        res.end();
+        return;
+    }
+    var filename = path.join(process.cwd(), url.parse(req.url).pathname);
+    var q = filename.indexOf('?');
+    if(q>=0) filename = filename.substring(0,q);
+    if(filename.endethWith('/')) filename += 'index.html';
 
-    path.exists(filePath, function(exists){
-
-        if(exists){
-            fs.readFile(filePath, function(error, content) {
-                if(!error){
-                    response.writeHead(200, { 'Content-Type': contentType(filePath) });
-                    response.end(content, 'utf-8');
-                    console.log('200 '+filePath);
-                }
-                else{
-                    response.writeHead(500);
-                    response.end();
-                    console.log('500 '+filePath);
-                }
-            });
+    path.exists(filename, function(exists){
+        if(!exists) {
+            res.writeHead(404);
+            res.end();
+            console.log('404 '+filename);
+            return;
         }
-        else{
-            response.writeHead(404);
+        var mimeType = mimeTypes[path.extname(filename)];
+        if(!mimeType) mimeType='text/plain';
+        res.writeHead(200, { 'Content-Type': mimeType });
+
+        var fileStream = fs.createReadStream(filename);
+        fileStream.setEncoding('utf-8');
+        fileStream.on('error', function(){
+            response.writeHead(500);
             response.end();
-            console.log('404 '+filePath);
-        }
+            console.log('500 '+filename);
+        });
+        fileStream.pipe(res);
+        console.log('200 '+filename);
     });
-
-}).listen(8080, "127.0.0.1");
-
-function contentType(filePath){
-    return filePath.endethWith('.html')? 'text/html':
-           filePath.endethWith('.js')?   'application/javascript':
-           filePath.endethWith('.css')?  'text/css':
-           filePath.endethWith('.json')? 'application/json':
-                                         'text/plain';
-}
+}).listen(8080);
 
 String.prototype.startethWith = function(str){ return this.slice(0, str.length)==str; };
 String.prototype.endethWith   = function(str){ return this.slice(  -str.length)==str; };
