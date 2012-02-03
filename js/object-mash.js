fb4842420d4efb6b6c317086de51c2a5052610a9
Duncan Cragg <object.network@cilux.org>
2012-02-03 15:54:45 +0000

    decamelise. thx again to stackoverflow. i luv u guyz.

--- js/object-mash.js
+++ js/object-mash.js
@@ -138,7 +138,7 @@ function JSON2HTML() {
             return date;
         },
         deCamelise: function(s){
-            return s;
+            return s.replace(/([a-z])([A-Z])/g, '$1 $2').replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3').replace(/^./, function(str){ return str.toUpperCase(); });
         }
     };
 };
